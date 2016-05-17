070171f53294a370a615c2000238fe121a1321e3
Jason Keene <jasonkeene@gmail.com>
2016-05-17 09:29:52 -0600

    Clean the equal lines matcher up a bit

--- src/anubot/bot/equal_lines_matcher_test.go
+++ src/anubot/bot/equal_lines_matcher_test.go
@@ -8,14 +8,12 @@ import (
 )
 
 type equalLines struct {
-	expected []byte
 	matchers.EqualMatcher
 }
 
 func EqualLines(lines ...string) types.GomegaMatcher {
 	expected := []byte(strings.Join(lines, "\r\n") + "\r\n")
-	return &equalLines{
-		expected:     expected,
-		EqualMatcher: matchers.EqualMatcher{Expected: expected},
-	}
+	matcher := &equalLines{}
+	matcher.Expected = expected
+	return matcher
 }

dde5098fdc4db846d32513e5ef21c1584e176230
Jason Keene <jasonkeene@gmail.com>
2016-05-22 16:07:30 -0600

    Finish listeners datastructure

---
+++ app/spec/listeners_spec.js
@@ -0,0 +1,48 @@
+
+const Listeners = require("../lib/listeners.js");
+
+describe("Listeners", () => {
+    var listeners,
+        mockListener;
+
+    beforeEach(() => {
+        listeners = new Listeners();
+        mockListener = jasmine.createSpy("mockListener");
+    });
+
+    it("can add listeners that respond to commands by name", () => {
+        var uncalledMockListener = jasmine.createSpy("mockListener");
+
+        listeners.add("test-command", mockListener);
+        listeners.dispatch("test-command", "test-payload");
+        expect(mockListener).toHaveBeenCalledWith("test-payload");
+        expect(uncalledMockListener).not.toHaveBeenCalled();
+    });
+
+    it("does not dispatch commands that have no listeners", () => {
+        listeners.add("test-command", mockListener);
+
+        listeners.dispatch("bad-test-command", "test-payload");
+        expect(mockListener).not.toHaveBeenCalled();
+    });
+
+    it("can remove listeners by command name", () => {
+        // dispatch an event
+        listeners.add("test-command", mockListener);
+        listeners.dispatch("test-command", "test-payload");
+        expect(mockListener).toHaveBeenCalledWith("test-payload");
+        mockListener.calls.reset();
+
+        // now remove this listener and it shouldn't get the second event
+        listeners.remove("test-command", mockListener);
+        listeners.dispatch("test-command", "test-payload");
+        expect(mockListener).not.toHaveBeenCalled();
+    });
+
+    it("does not attempt to remove listeners from unknown commands", () => {
+        listeners.add("test-command", mockListener);
+        listeners.remove("bad-test-command", mockListener);
+        listeners.dispatch("test-command", "test-payload");
+        expect(mockListener).toHaveBeenCalledWith("test-payload");
+    });
+});
---
+++ app/src/listeners.js
@@ -0,0 +1,38 @@
+
+const Listeners = function () {
+    this.listenersMap = {};
+}
+
+Listeners.prototype.add = function (cmd, listener) {
+    if (this.listenersMap[cmd] === undefined) {
+        this.listenersMap[cmd] = [];
+    }
+    this.listenersMap[cmd].push(listener);
+};
+
+Listeners.prototype.remove = function (cmd, listener) {
+    var l = this.listenersMap[cmd];
+    if (l === undefined) {
+        // no listeners for this cmd
+        return;
+    }
+    for (var i = 0; i < l.length; i++) {
+        var j = -1;
+        while ((j = l.indexOf(listener)) !== -1) {
+            l.splice(j, 1);
+        }
+    }
+};
+
+Listeners.prototype.dispatch = function (cmd, payload) {
+    var l = this.listenersMap[cmd];
+    if (l === undefined) {
+        // no listeners for this cmd
+        return;
+    }
+    for (var i = 0; i < l.length; i++) {
+        l[i](payload);
+    }
+};
+
+module.exports = Listeners;

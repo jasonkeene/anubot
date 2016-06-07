3aa13c3c818b4e81e76e892c8ce8c7904e372203
Jason Keene <jasonkeene@gmail.com>
2016-06-06 20:09:26 -0600

    Add building of binaries to the test script

--- test.sh
+++ test.sh
@@ -2,11 +2,22 @@
 
 set -ex
 
+echo building binaries
+
+mkdir -p bin
+ls src/anubot/cmd | while read line; do
+    go build -o bin/$line anubot/cmd/$line
+done
+
+echo running ginkgo tests
+
 pushd src/anubot
     ginkgo unfocus
     goimports -w .
     ginkgo -r -race -randomizeAllSpecs
 popd
+
+echo running jasmine tests
 
 pushd app
     jasmine

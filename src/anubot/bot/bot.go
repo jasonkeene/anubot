b941b1e9cf5e96f2bddf90cfe025dab0820729b7
Jason Keene <jasonkeene@gmail.com>
2016-09-24 23:02:25 -0600

    Rip out old bot code, stub out new design #22

--- src/anubot/bot/bot.go
+++ src/anubot/bot/bot.go
@@ -1,285 +1,112 @@
 package bot
 
 import (
-	"crypto/tls"
-	"errors"
+	"anubot/stream"
+	"encoding/json"
 	"log"
-	"net"
-	"strconv"
-	"strings"
 	"sync"
+	"time"
 
-	"github.com/fluffle/goirc/client"
+	"github.com/pebbe/zmq4"
 )
 
-var capString string
-
-func init() {
-	caps := []string{
-		"twitch.tv/tags",
-		"twitch.tv/commands",
-		"twitch.tv/membership",
-	}
-	capString = strings.Join(caps, " ")
+// Sender sends messsgaes to a stream source.
+type Sender interface {
+	Send(ms stream.TXMessage)
 }
 
-type ConnConfig struct {
-	StreamerUsername string
-	StreamerPassword string
-	BotUsername      string
-	BotPassword      string
-	Host             string
-	Port             int
-	TLSConfig        *tls.Config
-	Flood            bool
+// Feature accepts messages and tick signals that are used to implement bot
+// logic.
+type Feature interface {
+	HandleMessage(ms stream.RXMessage)
+	Tick()
 }
 
-// Bot communicates with the IRC server and has pointers to features.
+// Bot receives messages and takes actions based on those messages.
 type Bot struct {
-	mu           sync.Mutex
-	connected    bool
-	streamerConn *client.Conn
-	botConn      *client.Conn
-	connConfig   *ConnConfig
-
-	channelMu sync.Mutex
-	channel   string
-
-	// features
-	chatFeature *ChatFeature
+	sub        *zmq4.Socket
+	featuresMu sync.Mutex
+	features   []Feature
 }
 
-// Connect establishes two connections to the Twitch IRC server, one as the
-// streamer and one as the bot. It then joins the streamer's channel.
-func (b *Bot) Connect(c *ConnConfig) (chan struct{}, error) {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	// save off conn config
-	b.connConfig = c
-
-	// check to see if we are connected already
-	if b.connected {
-		return nil, errors.New("This bot is already connected, disconnect first.")
-	}
-
-	// fix up TLS config if not provided
-	initTLS(c)
-
-	// connect to the server
-	streamerConn, streamerDisconnected, err := connectUser(
-		c.StreamerUsername,
-		c.StreamerPassword,
-		c,
-	)
+// New returns a new Bot that is connected to publishers.
+func New(topic string, pubEndpoints []string) (*Bot, error) {
+	sub, err := zmq4.NewSocket(zmq4.SUB)
 	if err != nil {
 		return nil, err
 	}
-	botConn, botDisconnected, err := connectUser(
-		c.BotUsername,
-		c.BotPassword,
-		c,
-	)
-	if err != nil {
-		streamerConn.Quit()
-		return nil, err
-	}
-
-	b.connected = true
-	b.streamerConn = streamerConn
-	b.botConn = botConn
-
-	// signal disconnect on either bot or streamer connection
-	disconnected := make(chan struct{})
-	go func() {
-		defer close(disconnected)
-		for {
-			select {
-			case <-streamerDisconnected:
-				return
-			case <-botDisconnected:
-				return
-			}
+	for _, endpoint := range pubEndpoints {
+		err = sub.Connect(endpoint)
+		if err != nil {
+			return nil, err
 		}
-	}()
-
-	// join streamer's channel by default
-	b.channelMu.Lock()
-	b.channel = "#" + c.StreamerUsername
-	b.channelMu.Unlock()
-	b.join(b.channel)
-
-	// ask for capabilities
-	b.requestCapabilities()
-
-	// register handlers for features
-	b.chatFeature.Register()
-
-	return disconnected, nil
-}
-
-func (b *Bot) requestCapabilities() {
-	b.Send("streamer", "CAP REQ :"+capString)
-	b.Send("bot", "CAP REQ :"+capString)
-}
-
-// Disconnect tears down the connections to the IRC server and resets the state
-// of the bot so that you can connect again.
-func (b *Bot) Disconnect() {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-	b.connected = false
-
-	if b.streamerConn != nil {
-		b.streamerConn.Quit()
-		b.streamerConn = nil
 	}
-	if b.botConn != nil {
-		b.botConn.Quit()
-		b.botConn = nil
+	err = sub.SetSubscribe(topic)
+	if err != nil {
+		return nil, err
 	}
-	if b.connConfig != nil {
-		b.connConfig = nil
+
+	b := &Bot{
+		sub: sub,
 	}
+	return b, nil
 }
 
-// Join joins an IRC channel.
-func (b *Bot) Join(channel string) {
-	b.channelMu.Lock()
-	defer b.channelMu.Unlock()
-
-	b.join(channel)
-	b.part(channel)
-	b.channel = channel
+// Start spawns goroutines needed to handle messages.
+func (b *Bot) Start() {
+	go b.startSub()
+	go b.startTicker()
 }
 
-func (b *Bot) join(channel string) {
-	b.streamerConn.Join(channel)
-	b.botConn.Join(channel)
+// Stop tears down the goroutines needed to handle messages.
+func (b *Bot) Stop() {
+	// TODO: implement this
 }
 
-func (b *Bot) part(channel string) {
-	b.streamerConn.Part(b.channel)
-	b.botConn.Part(b.channel)
-}
+func (b *Bot) startSub() {
+	for {
+		rb, err := b.sub.RecvMessageBytes(0)
+		if err != nil {
+			log.Printf("messages not read, got err: %s", err)
+			continue
+		}
+		if len(rb) < 2 {
+			log.Printf("received message bytes had invalid length: %#v", rb)
+			continue
+		}
+		var ms stream.RXMessage
+		err = json.Unmarshal(rb[1], &ms)
+		if err != nil {
+			log.Printf("could not unmarshal, got err: %s", err)
+			continue
+		}
 
-// Channel returns the currently active channel.
-func (b *Bot) Channel() string {
-	b.channelMu.Lock()
-	defer b.channelMu.Unlock()
-	return b.channel
-}
-
-// StreamerUsername returns the streamer's username.
-func (b *Bot) StreamerUsername() string {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	if b.connConfig == nil {
-		return ""
-	}
-	return b.connConfig.StreamerUsername
-}
-
-// BotUsername returns the bot's username.
-func (b *Bot) BotUsername() string {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-
-	if b.connConfig == nil {
-		return ""
-	}
-	return b.connConfig.BotUsername
-}
-
-// Send sends a raw message to the specified IRC connection.
-func (b *Bot) Send(user, message string) {
-	switch user {
-	case "streamer":
-		b.streamerConn.Raw(message)
-	case "bot":
-		b.botConn.Raw(message)
-	default:
-		log.Panicf("Bad user provided for sending message")
-	}
-}
-
-// Privmsg sends a chat message to the specified IRC connection.
-func (b *Bot) Privmsg(user, target, message string) {
-	switch user {
-	case "streamer":
-		b.streamerConn.Privmsg(target, message)
-	case "bot":
-		b.botConn.Privmsg(target, message)
-	default:
-		log.Panicf("Bad user provided for sending message")
-	}
-}
-
-// HandleFunc registers functions to handle IRC commands for a specific user.
-func (b *Bot) HandleFunc(user, command string, handlefunc client.HandlerFunc) {
-	switch user {
-	case "streamer":
-		b.streamerConn.HandleFunc(command, handlefunc)
-	case "bot":
-		b.botConn.HandleFunc(command, handlefunc)
-	default:
-		log.Panicf("Bad user provided for registering handlefunc")
-	}
-}
-
-// InitChatFeature wires up the chat feature of the bot.
-func (b *Bot) InitChatFeature(dispatcher *MessageDispatcher) {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-	b.chatFeature = NewChatFeature(b, dispatcher)
-}
-
-// ChatFeature returns the chat feature of the bot.
-func (b *Bot) ChatFeature() *ChatFeature {
-	b.mu.Lock()
-	defer b.mu.Unlock()
-	return b.chatFeature
-}
-
-func initTLS(c *ConnConfig) {
-	if c.TLSConfig == nil {
-		c.TLSConfig = &tls.Config{
-			ServerName: c.Host,
+		var fs []Feature
+		b.featuresMu.Lock()
+		copy(fs, b.features)
+		b.featuresMu.Unlock()
+		for _, f := range fs {
+			f.HandleMessage(ms)
 		}
 	}
 }
 
-func connectUser(username, password string, c *ConnConfig) (*client.Conn, chan struct{}, error) {
-	// create client
-	cfg := client.NewConfig(username)
-	cfg.Me.Name = username
-	cfg.Me.Ident = "anubot"
-	cfg.Pass = password
-	cfg.SSL = true
-	cfg.SSLConfig = c.TLSConfig
-	cfg.Server = net.JoinHostPort(c.Host, strconv.Itoa(c.Port))
-	cfg.Flood = c.Flood
-	conn := client.Client(cfg)
-
-	// register dis/connect handlers
-	connected := make(chan struct{})
-	disconnected := make(chan struct{})
-	registerSignalHandler(client.CONNECTED, conn, connected)
-	registerSignalHandler(client.DISCONNECTED, conn, disconnected)
-
-	if err := conn.Connect(); err != nil {
-		return nil, nil, err
+func (b *Bot) startTicker() {
+	t := time.NewTicker(time.Second)
+	for range t.C {
+		var fs []Feature
+		b.featuresMu.Lock()
+		copy(fs, b.features)
+		b.featuresMu.Unlock()
+		for _, f := range fs {
+			f.Tick()
+		}
 	}
-	<-connected
-	return conn, disconnected, nil
 }
 
-func registerSignalHandler(event string, conn *client.Conn, signal chan struct{}) {
-	// TODO: get remover working to prevent closing signal chan multiple times
-	//var remover client.Remover
-	conn.HandleFunc(event, func(conn *client.Conn, line *client.Line) {
-		close(signal)
-		//remover.Remove()
-	})
+// AddFeature adds a feature to accept messages and ticks.
+func (b *Bot) AddFeature(f Feature) {
+	b.featuresMu.Lock()
+	b.featuresMu.Unlock()
+	b.features = append(b.features, f)
 }
